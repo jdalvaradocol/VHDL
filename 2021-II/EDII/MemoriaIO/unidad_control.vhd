02321644336efc53497461b2a4b5d2cd5ea1e462
Jose David <80493134+jdalvaradocol@users.noreply.github.com>
2022-02-25 12:24:07 -0500

    Update unidad_control.vhd

--- 2021-II/EDII/MemoriaIO/unidad_control.vhd
+++ 2021-II/EDII/MemoriaIO/unidad_control.vhd
@@ -1,4 +1,3 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
@@ -35,45 +34,45 @@ begin
 		if rising_edge(clk_i) then
 		
 		
-			if estado = 0 then
-			
-				add_ram_o <= "00";
-				WR_o <= '1';
-				data_o <= data_sw_i;
-				
-				estado <= estado + 1;
-				
-			elsif	estado = 1 then
+			if estado < 1 then
 			
 				add_ram_o <= "01";
 				WR_o <= '0';
-				
 				estado <= estado + 1;
-			
-			elsif	estado = 2 then
+								
+			elsif	estado < 3 then
+
+				add_ram_o <= "00";
+				WR_o <= '1';
+				data_o <= data_sw_i;
+				estado <= estado + 1;
+					
+			elsif	estado < 5 then
 			
 				add_ram_o <= "00";
 				WR_o <= '0';
+				data_o <= "00000000";
 				estado <= estado + 1;
-			
-			elsif	estado = 3 then
+				
+		   elsif	estado < 7 then
 			
 				operacion <= conv_integer(data_ram_i) + 10;
-			
 				estado <= estado + 1;
-			
-			elsif	estado = 4 then
+				
+			elsif	estado < 9 then
 			
 				add_ram_o <= "01";
 				WR_o <= '1';
 				data_o <= std_logic_vector(to_unsigned(operacion,data_o'length));
-				estado <= estado + 1;
-			
-			elsif	estado = 5 then
-			
+				estado <= estado + 1;							
+
+			elsif	estado < 11 then
+				
+				add_ram_o <= "11";
 				WR_o <= '0';
 				data_o <= "00000000";
 				estado <= 0;
+			   operacion<=0;
 				
 			end if;
 			
