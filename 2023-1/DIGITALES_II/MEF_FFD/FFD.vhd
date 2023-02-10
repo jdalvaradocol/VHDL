4932843053cb2caf21a4b3f58515a4f28f612432
Jose David <80493134+jdalvaradocol@users.noreply.github.com>
2023-02-10 15:53:01 -0500

    MEF_FFD

---
+++ 2023-1/DIGITALES_II/MEF_FFD/FFD.vhd
@@ -0,0 +1,29 @@
+-- Quartus II VHDL Template
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity FFD is
+	port 
+	(
+		D_i, clk_i 	:in 	std_logic;
+		Q_o			:out 	std_logic		
+	);
+end FFD;
+
+architecture Behavioral of FFD is
+
+begin
+
+	FF_D: process(clk_i, D_i)
+	begin	
+		if rising_edge(clk_i) then
+			Q_o <= D_i;
+		end if;
+	end process FF_D;
+
+end Behavioral;
+
+
+
+
---
+++ 2023-1/DIGITALES_II/MEF_FFD/MEF_FFD.vhd
@@ -0,0 +1,53 @@
+-- Quartus II VHDL Template
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity MEF_FFD is
+	port 
+	(
+		X_i, clk_i : in  std_logic;
+		Y_o		   : out std_logic_vector(2 downto 0);
+		E_o		   : out  std_logic_vector(1 downto 0)
+	);
+end MEF_FFD;
+
+architecture Behavioral of MEF_FFD is
+
+signal FB, FA, A, B : std_logic:= '0';	
+
+begin
+
+	FFD_A: entity work.FFD port map
+	(
+		clk_i 	=> 	clk_i,
+		D_i		=>	FA,	
+		Q_o		=>	A
+	);
+
+	FFD_B: entity work.FFD port map
+		(
+			clk_i 	=> 	clk_i,
+			D_i		=>	FB,	
+			Q_o		=>	B
+		);
+
+FA <= (not(A) and not(B) and     (X_i)) or 
+	  (not(A) and    (B) and not (X_i)) or
+	  (   (A) and    (B) and     (X_i)) or
+	  (   (A) and not(B) and not (X_i)) ;
+
+FB <= not B;
+
+E_o(0) <= A;
+E_o(1) <= B;
+
+Y_o(0) <= not(A) and not(b); 
+Y_o(1) <= B;
+Y_o(2) <= A;
+
+end Behavioral;
+
+
+
+
