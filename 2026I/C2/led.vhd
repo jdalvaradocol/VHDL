6e1ab77919fe6e1c21e0e072d3aaa19784a43fa1
Jose David <80493134+jdalvaradocol@users.noreply.github.com>
2026-03-06 16:27:02 -0500

    main

---
+++ 2026I/C2/led.vhd
@@ -0,0 +1,30 @@
+-- Quartus II VHDL Template
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity led is
+	port 
+	(
+		A_i : in  std_logic;
+		B_i : in  std_logic;
+		C_i : in  std_logic;
+		x_o : out std_logic
+
+	);
+end led;
+
+architecture Behavioral of led is
+
+begin
+
+x_o <= 	(not(A_i) and not(B_i) and not(C_i)) or
+			(not(A_i) and    (B_i) and    (C_i)) or
+			(   (A_i) and not(B_i) and not(C_i)) or
+			(   (A_i) and    (B_i) and    (C_i)) ;
+
+end Behavioral;
+
+
+
+
